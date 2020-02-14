142c6005a24b78d2fc0667cbd4beb117d95ef1f1
Grégoire Lurton <gregoire.lurton@gmail.com>
2020-02-14 19:19:47 +0100

    Initial Commit

---
+++ get_outpatients.sql
@@ -0,0 +1,27 @@
+-- @block Bookmarked query
+-- @group SNIS DRC
+-- @name get monthly sum of revenue by Province
+
+-- SELECT SUM(CAST(DV.value AS INT)) AS value,
+SELECT CAST(DV.value AS INT) AS value,
+       OU.namelevel2 AS province_name,
+       periods.enddate AS period_end,
+       periods.iso AS period,
+       OU.uidlevel3 AS zone_id,
+       OU.uidlevel5 AS fosa_id, 
+       DE.name AS data_element_name
+FROM datavalue DV
+JOIN dataelement DE
+  ON DE.dataelementid = DV.dataelementid
+JOIN _periodstructure PERIODS
+  ON DV.periodid = PERIODS.periodid
+JOIN public._orgunitstructure OU
+  ON DV.sourceid = OU.organisationunitid
+JOIN public.categoryoptioncombo COC
+  ON DV.categoryoptioncomboid = COC.categoryoptioncomboid
+WHERE (DE.name LIKE '%A 1.1 %') -- AND DE.name LIKE '%Nouveaux%')
+  AND periods.enddate > '20170101'
+  AND periods.enddate < '20191231'
+--GROUP BY periods.enddate,
+--         OU.namelevel2,
+--         OU.uidlevel5
